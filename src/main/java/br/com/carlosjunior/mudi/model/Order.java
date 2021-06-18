be1f5bd159a2c77a8994d8937c48dd5c8bf2e817
Carlos Cazelatto Junior <carlosjunior1983@gmail.com>
2021-06-18 10:19:25 -0300

    First config JPA with Order class

--- src/main/java/br/com/carlosjunior/mudi/model/Order.java
+++ src/main/java/br/com/carlosjunior/mudi/model/Order.java
@@ -3,8 +3,20 @@ package br.com.carlosjunior.mudi.model;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tb_order")
 public class Order {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
 	private String productName;
 	private BigDecimal productValue;
 	private LocalDate dateDelivery;
