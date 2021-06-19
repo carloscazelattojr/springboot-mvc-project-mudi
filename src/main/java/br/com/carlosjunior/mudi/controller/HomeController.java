78e375142fb47d7bc7248c6c38789797034431d0
Carlos Cazelatto Junior <carlosjunior1983@gmail.com>
2021-06-19 06:10:10 -0300

    Created Order Status Routes

--- src/main/java/br/com/carlosjunior/mudi/controller/HomeController.java
+++ src/main/java/br/com/carlosjunior/mudi/controller/HomeController.java
@@ -5,30 +5,47 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.carlosjunior.mudi.model.Order;
+import br.com.carlosjunior.mudi.model.StatusOrder;
 import br.com.carlosjunior.mudi.repositories.OrderRepository;
 
 @Controller
+@RequestMapping("/")
 public class HomeController {
 
 	@Autowired
-	private OrderRepository orderRepository; 
- 
+	private OrderRepository orderRepository;
+
 	@GetMapping
 	public String homeMain() {
-		return  "redirect:/home";
+		return "redirect:/home";
 	}
- 
-	
-	@GetMapping("/home")
-	public String home(Model model ) {
 
+	@GetMapping("/home")
+	public String home(Model model) {
 		List<Order> orders = orderRepository.findAll();
 		model.addAttribute("orders", orders);
-		
 		return "home";
+	}
+
+	@GetMapping("/home/{status}")
+	public String homeStatusOrders(@PathVariable("status") String status, Model model) {
+
+		List<Order> orders = orderRepository.findByStatus(StatusOrder.valueOf(status.toUpperCase()));
+		model.addAttribute("orders", orders);
+		model.addAttribute("status", status);
+
+		return "home";
+	}
+
+	@ExceptionHandler(IllegalArgumentException.class)
+	public String onError() {
+		return "redirect:/home";
 	}
 
 }
--- src/main/java/br/com/carlosjunior/mudi/repositories/OrderRepository.java
+++ src/main/java/br/com/carlosjunior/mudi/repositories/OrderRepository.java
@@ -1,12 +1,17 @@
 package br.com.carlosjunior.mudi.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.carlosjunior.mudi.model.Order;
+import br.com.carlosjunior.mudi.model.StatusOrder;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+	List<Order> findByStatus(StatusOrder status);
 
 
 }
